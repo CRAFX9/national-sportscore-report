715d3287ba8a7c24f158ec87363ed3ecb1368c91
CRAFX9 <crafx9@gmail.com>
2026-07-16 17:10:21 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,7 +102,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
